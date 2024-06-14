0f9d1c41dcb324bda09fba6e80e3531aa8ba8a2d
mirjanaglisovic <mirjana.glisovic123@gmail.com>
2024-06-14 23:13:08 +0200

    Pagination

---
+++ src/components/Pagination/Pagination.jsx
@@ -0,0 +1,31 @@
+/* eslint-disable react/prop-types */
+import "./Pagination.css";
+import { FaChevronLeft, FaChevronRight } from "react-icons/fa";
+
+function Pagination({ numOfPages, page, setPage }) {
+  const arr = [];
+  for (let i = 1; i <= numOfPages; i++) {
+    arr.push(i);
+  }
+  return (
+    <div className="pagination">
+      <button className="pagination-btn" onClick={() => setPage(page - 1)}>
+        <FaChevronLeft height={25} />
+      </button>
+      {arr.map((num) => (
+        <button
+          className="pagination-btn"
+          key={num}
+          onClick={() => setPage(num)}
+        >
+          {num}
+        </button>
+      ))}
+      <button className="pagination-btn" onClick={() => setPage(page + 1)}>
+        <FaChevronRight height={25} />
+      </button>
+    </div>
+  );
+}
+
+export default Pagination;
--- src/pages/Hotels/Hotels.jsx
+++ src/pages/Hotels/Hotels.jsx
@@ -1,21 +1,35 @@
 import HotelCard from "../../components/Cards/HotelCard/HotelCard";
 import hotels from "../../common/hotels.json";
 import "./Hotels.css";
+import Pagination from "../../components/Pagination/Pagination";
+import { useState } from "react";
 
 function Hotels() {
+  const [page, setPage] = useState(1);
+  const numOfHotelsPerPage = 8;
+  const numOfHotels = hotels.length;
+  const numOfPages = Math.ceil(numOfHotels / numOfHotelsPerPage);
   return (
-    <div className="hotels">
-      {hotels.map((hotel) => (
-        <HotelCard
-          key={hotel.id}
-          imageUrl={hotel.imageUrl}
-          title={hotel.title}
-          content={hotel.content}
-          total={hotel.total}
-          onClick={() => (window.location.href = `/hotels/${hotel.id}`)}
-        />
-      ))}
-    </div>
+    <>
+      <div className="hotels">
+        {hotels
+          .map((hotel) => (
+            <HotelCard
+              key={hotel.id}
+              imageUrl={hotel.imageUrl}
+              title={hotel.title}
+              content={hotel.content}
+              total={hotel.total}
+              onClick={() => (window.location.href = `/hotels/${hotel.id}`)}
+            />
+          ))
+          // .slice(0, 8) // prikazivanje prvih 8 hotela  111111
+          //.slice(8, 16) // prikazivanje drugih 8 hotela 222222
+          // .slice(16, 24) // prikazivanje trecih 8 hotela  333333
+          .slice(numOfHotelsPerPage * (page - 1), numOfHotelsPerPage * page)}
+      </div>
+      <Pagination numOfPages={numOfPages} page={page} setPage={setPage} />
+    </>
   );
 }
 
